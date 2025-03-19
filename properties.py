175d4f8a35314661bcaf912504c80e2e9698adfe
bekxwt <bekzatbakytbek03@gmail.com>
2025-03-19 22:08:29 +0600

    Updated project tree

--- properties.py
+++ properties.py
@@ -1,68 +1,65 @@
 import bpy
 from bpy.types import PropertyGroup
-from bpy.props import (
-    PointerProperty,
-    CollectionProperty,
-    IntProperty,
-    EnumProperty,
-    StringProperty,
-    FloatProperty
-)
-from .utils import save_prefs, load_prefs
+from bpy.props import *
+from .utils import load_prefs, save_prefs
 
 class LODItem(PropertyGroup):
     lod_object: PointerProperty(type=bpy.types.Object)
 
-def register():
-    bpy.utils.register_class(LODItem)
-    
-    # LOD System
-    bpy.types.Object.lod_items = CollectionProperty(type=LODItem)
-    bpy.types.Object.lod_index = IntProperty(default=0)
-    
-    # Preferences
-    bpy.types.Scene.selected_export_format = EnumProperty(
-        name="Export Format",
+# Custom scene properties
+class EngineExportSettings(PropertyGroup):
+    export_format: EnumProperty(
         items=[
-            ('FBX', "FBX", "FBX format"),
-            ('GLTF', "GLTF", "GLTF format"),
-            ('OBJ', "OBJ", "OBJ format")
+            ('FBX', "FBX", "Autodesk FBX format"),
+            ('GLTF', "GLTF", "GL Transmission Format"),
+            ('OBJ', "OBJ", "Wavefront OBJ")
         ],
         default='FBX',
         update=lambda s,c: save_prefs()
     )
     
-    bpy.types.Scene.export_folder = StringProperty(
-        name="Export Folder",
+    export_folder: StringProperty(
         subtype='DIR_PATH',
         update=lambda s,c: save_prefs()
     )
     
-    bpy.types.Scene.lod_default_ratio = FloatProperty(
-        name="LOD Default Ratio",
-        default=0.5,
-        min=0.0,
-        max=1.0,
+    export_apply_modifiers: BoolProperty(
+        name="Apply Modifiers",
+        default=True,
         update=lambda s,c: save_prefs()
     )
     
-    bpy.types.Scene.merge_distance = FloatProperty(
+    lod_default_ratio: FloatProperty(
+        name="LOD Ratio",
+        min=0.01, max=1.0,
+        default=0.5,
+        update=lambda s,c: save_prefs()
+    )
+    
+    merge_distance: FloatProperty(
         name="Merge Distance",
+        min=0.0, soft_max=0.1,
         default=0.001,
-        min=0.0,
-        step=0.0001,
         precision=4,
         update=lambda s,c: save_prefs()
     )
     
+    auto_smooth_angle: FloatProperty(
+        name="Auto Smooth Angle",
+        min=0.0, max=180.0,
+        default=30.0,
+        subtype='ANGLE',
+        update=lambda s,c: save_prefs()
+    )
+
+def register():
+    bpy.utils.register_class(LODItem)
+    bpy.utils.register_class(EngineExportSettings)
+    bpy.types.Scene.engine_export = PointerProperty(type=EngineExportSettings)
     load_prefs()
 
 def unregister():
     save_prefs()
-    del bpy.types.Object.lod_items
-    del bpy.types.Object.lod_index
-    del bpy.types.Scene.selected_export_format
-    del bpy.types.Scene.export_folder
-    del bpy.types.Scene.lod_default_ratio
-    del bpy.types.Scene.merge_distance
+    bpy.utils.unregister_class(EngineExportSettings)
     bpy.utils.unregister_class(LODItem)
+    del bpy.types.Scene.engine_export
